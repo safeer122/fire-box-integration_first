7ab21f6f87239412f8c65022701c336fa66b897b
safeer122 <safeerpookuth6677@gmail.com>
2023-11-04 13:43:10 +0530

    third commit

--- lib/Ui/add_post.dart
+++ lib/Ui/add_post.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:firebase_base/Ui/toast_message.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +15,7 @@ class _addpostState extends State<addpost> {
   final postController=TextEditingController();
   bool loading =false;
   final databaseRef = FirebaseDatabase.instance.ref('Post');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,55 +23,67 @@ class _addpostState extends State<addpost> {
         backgroundColor: Colors.blue,
         title: Text("add Post"),
         automaticallyImplyLeading: false,
+        centerTitle: true,
       ),
       body: Padding(
-        padding:EdgeInsets.only(left: 20.w),
-        child: Column(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Stack(
           children: [
-            SizedBox(height: 30.h,),
-            TextFormField(
-              maxLines: 4,
-                  controller: postController,
-                  decoration: InputDecoration(
-                    hintText: "Add whats in your mind",
-                    border: OutlineInputBorder()
+            Column(
+              children: [
+                SizedBox(height: 30.h,),
+                TextFormField(
+                  maxLines: 4,
+                      controller: postController,
+                      decoration: InputDecoration(
+                        hintText: "Add whats in your mind",
+                        border: OutlineInputBorder()
+                      ),
+                ),
+                SizedBox(height: 30.h,),
+                GestureDetector(onTap: (){
+                  setState(() {
+                    loading=true;
+
+                  });
+                  String id =DateTime.now().millisecondsSinceEpoch.toString();
+                  databaseRef.child(id).set( {
+                    'title':postController.text.toString(),
+                    'id':id,
+                  }).then((value) {
+                  ToastMessage().toastmessage(message: 'post added');
+                  setState(() {
+                    loading=false;
+
+                  });
+                  }).onError((error, stackTrace){
+                    ToastMessage()
+                        .toastmessage(message: error.toString());
+                    setState(() {
+                      loading=false;
+                    });
+                },);},
+                  child: Container(
+                    width: 200.w,
+                    height: 50.h,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(color: Colors.blue,borderRadius: BorderRadius.circular(30.sp)),
+                    child: Text("Add",style: TextStyle(
+                      fontSize: 20.sp,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xffFFFFFF),
+                    ),),
                   ),
-            ),
-            SizedBox(height: 30.h,),
-            GestureDetector(onTap: (){
-              databaseRef.child(DateTime.now().month.toString()).child('comments').set( {
-                'title':postController.text.toString(),
-                'id':DateTime.now().day.toString(),
-              }).then((value) {
-              ToastMessage().toastmessage(message: 'post added');
-              setState(() {
 
-              });
-              }).onError((error, stackTrace){
-                ToastMessage()
-                    .toastmessage(message: error.toString());
-                setState(() {
+                ),
 
-                });
-            },);},
-              child: Container(
-                width: 200.w,
-                height: 50.h,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(color: Colors.blue,borderRadius: BorderRadius.circular(30.sp)),
-                child: Text("Add",style: TextStyle(
-                  fontSize: 20.sp,
-                  fontWeight: FontWeight.w600,
-                  color: Color(0xffFFFFFF),
-                ),),
-              ),
-
-            ),
+              ],
+            ),Visibility(visible: loading,child: CircularProgressIndicator()),
 
           ],
         ),
-
       ),
     );
   }
+
 }
--- lib/Ui/home.dart
+++ lib/Ui/home.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_base/Ui/add_post.dart';
+import 'package:firebase_base/Ui/toast_message.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 class home extends StatefulWidget {
   const home({super.key});
 
@@ -9,22 +14,168 @@ class home extends StatefulWidget {
 }
 
 class _homeState extends State<home> {
+  final auth = FirebaseAuth.instance;
+
+  final ref = FirebaseDatabase.instance.ref('Post');
+  final searchfilter = TextEditingController();
+  final editcontroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title: Center(child: Text('Home')),
-       automaticallyImplyLeading: false,
-        
+        title: Text('Home'),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
       ),
-      body:Padding(
-        padding:EdgeInsets.only(left: 250.w,top: 500.h),
-        child: FloatingActionButton(onPressed: (){
-           Navigator.push(context,MaterialPageRoute(builder:(_)=>addpost() ));
+      body: Column(
+        children: [
+          SizedBox(
+            height: 10.h,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextFormField(
+              controller: searchfilter,
+              decoration: InputDecoration(
+                  hintText: ('search'), border: OutlineInputBorder()),
+              onChanged: (String value) {
+                setState(() {});
+              },
+            ),
+          ),
+          Expanded(
+            child: FirebaseAnimatedList(
+                query: ref,
+                defaultChild: Text('loading'),
+                itemBuilder: (context, snapshot, animation, index) {
+                  final title = snapshot.child('title').value.toString();
+                  if (searchfilter.text.isEmpty) {
+                    return ListTile(
+                      title: Text(snapshot.child('title').value.toString()),
+                      subtitle: Text(snapshot.child('id').value.toString()),
+                      trailing: PopupMenuButton(
+                        icon: Icon(Icons.more_vert),
+                        itemBuilder: (context)=>[
+                          PopupMenuItem(
+                            value: 1,
+                            onTap: (){
+
+                              showmydialoge(title,snapshot.child('id').value.toString());
+                            },
+                            child: ListTile(
+                              leading: Icon(Icons.edit),
+                              title: Text('edit'),
+                            ),
+                          ),
+                          PopupMenuItem(
+                            value: 1,
+                            child: ListTile(
+                              onTap: (){
+                                Navigator.pop(context);
+                                ref.child(snapshot.child('id').value.toString()).remove();
+                              },
+                              // onTap: (){
+                              //   Navigator.pop(context);
+                              //   ref.child(snapshot.child('id').value.toString()).remove();
+                              // },
+                              leading: Icon(Icons.delete),
+                              title: Text('delete'),
+                            ),
+                          )
+                        ],
+                      ),
+
+                    );
+
+                  } else if (title
+                      .toLowerCase()
+                      .toLowerCase()
+                      .contains(searchfilter.text.toLowerCase())) {
+                    return ListTile(
+                      title: Text(snapshot.child('title').value.toString()),
+                      subtitle: Text(snapshot.child('id').value.toString()),
+                    );
+                  } else {
+                    return Container();
+                  }
+                }),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (_) => addpost()));
         },
-        child: Icon(Icons.add),),
+        child: Icon(Icons.add),
       ),
     );
   }
+  Future<void> showmydialoge(String title,String id)async{
+    editcontroller.text=title;
+    return showDialog(
+        context: context,
+        builder: (BuildContext context){
+          return AlertDialog(
+            title: Text('update'),
+            content: Container(
+              child: TextField(
+                controller: editcontroller,
+                decoration: InputDecoration(
+                  hintText: 'edit here'
+                ),
+              ),
+            ),
+            actions: [
+              TextButton(onPressed: (){
+                Navigator.pop(context);
+              }, child: Text('cancel')),
+              TextButton(onPressed: (){
+                Navigator.pop(context);
+                ref.child(id).update({
+                  'title':editcontroller.text.toLowerCase()
+                }).then((value) {
+                  ToastMessage()
+                      .toastmessage(message:('post updated'));
+                }).onError((error, stackTrace){
+                  ToastMessage()
+                      .toastmessage(message: error.toString());
+                });
+              }, child: Text('update'))
+            ],
+          );
+        }
+    );
+
+  }
 }
+// Expanded(
+//     child:
+//
+//     StreamBuilder(
+//       stream:ref.onValue,
+//       builder: (context,snapshot){
+//
+//         if(!snapshot.hasData){
+//           return CircularProgressIndicator();
+//         }else {
+//           Map<dynamic, dynamic> map = snapshot.data!.snapshot
+//               .value as dynamic;
+//           List<dynamic>list = [];
+//           list.clear();
+//           list = map.values.toList();
+//
+//
+//           return ListView.builder(
+//               itemCount: snapshot.data!.snapshot.children.length,
+//               itemBuilder: (context, index) {
+//               return  ListTile(
+//                   title: Text(list[index]['title']),
+//                 subtitle: Text(list[index]['id']),
+//                 );
+//               });
+//         }
+//     },
+//
+//     )),
